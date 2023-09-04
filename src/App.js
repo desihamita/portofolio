e1e1749020e53ef49235e839ab330a344f559d9c
desihamita <69859667+desihamita@users.noreply.github.com>
2023-09-04 16:56:27 +0700

    fixing deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Services from "./components/services/Services";
 import Experience from "./components/experience/Experience";
 import Works from "./components/works/Works";
 import Portofolio from "./components/portofolio/Portofolio";
-import Testimonial from "./components/testimonial/Testimonial";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import { useContext } from "react";
